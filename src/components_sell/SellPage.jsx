74c863d2030f36ffaa0a6c1bedd45bb6ffa9c4b6
LeeKangryong <72414769+LeeKangRyong@users.noreply.github.com>
2024-06-01 23:00:00 +0900

    Update SellPage.jsx

--- src/components_sell/SellPage.jsx
+++ src/components_sell/SellPage.jsx
@@ -9,29 +9,20 @@ import axios from "axios";
 const SellPage = ({user_id}) => {
   const navigate = useNavigate();
   const [product, setProduct] = useState([]);
+  const [post_id, setPostId] = useState(null);
 
   useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await axios.get('http://localhost:8080/api/product'); // API 엔드포인트 변경 필요
-  //       setProduct(response.data);
-  //     } catch (error) {
-  //       console.error("API 호출 에러:", error);
-  //     }
-  //   };
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('127.0.0.1:8080/post/getpost/{post_id}'); // API 엔드포인트 변경 필요
+        setProduct(response.data);
+      } catch (error) {
+        console.error("API 호출 에러:", error);
+      }
+    };
 
-  //   fetchData();
-  // }, [user_id]);
-
-  const mockProduct = [
-    { title: '뉴발란스996',  price: 10000, size: 'L', used: 'nono', user_id:3,
-    brand_id: 3, category_id: 4, location:'동작', delivery:'안됨',exchange:"안됨",
-    post_id: 3, body:'sadfsdhflkasjfdhsdl;afjkasl;dfkjsa;ldkfjasld;fkjasdsadlfkjsaldfkjlajs;ldfksajd;flkㅣㅁㄴ;ㅏ얼;ㅣㅏㅁㄴ얼;ㅣ남얼;ㅣㅇㄴㅁ츠ㅟ;ㅜㅏㅓ;adsfhlaw;eflasl;fdkjv;lskafjdglfdkdkjvndskfbhadkfjvndslkfjvbndk;fjbnad;flkjvar[oghrwoiuahsdklvjdflvkjsdflkjhasdfklgjadskljvsdbnfkjadbg;krajghw;rugh;aewrghaudfkvand;ㅁ니어론미ㅏㅇ러니마어롲ㅂ댜려ㅗㅁㅈ뎌ㅗㅎ  ㅈㄷ힘뎢ㄱ헤;ㄷㄱㅈ해34ㅗㅔ9ㅅ8ㅎ9ㅔㅎㄷ괨녀ㅑㅗㅁ;나엄;s;fklvjanwr;kgjwe;gkbnfda;kjvb;fakjbvds;kfja;rkj];lfkjsa;ldkfjsal;dkfjsa;ldkfjas;lkfaskldfjsahflsㅣㅓ낭;ㅁ라ㅣ러닝ㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇㅇ2932#$^$%&^*%&^(*&)(*&%^$%#$@!dkfhsk',
-    img_urls:'https://mediaservice.audi.com/media/fast/H4sIAAAAAAAAAFvzloG1tIiBOTrayfuvpGh6-m1zJgaGigIGBgZGoDhTtNOaz-I_2DhCHsCEtzEwF-SlMwJZKUycmbmJ6an6QD4_I3taTmV-aUkxO0grj4OJvKrLmc65T3rWiLaKlnFop_H2MLACdTFqAQnmQiDB9wZIcCoxgEmQeQUgIgHEZ3JiZmBgrQAyIhlAgI-vtCinILEoMVevPDOlJENQw4BIIMzu4hri6OkTDACYh-QJ6QAAAA?wid=850'}
-  ];
-
-  setProduct(mockProduct);
-}, [user_id]);
+    fetchData();
+  }, [user_id]);
 
 const clickFavoriteButton = async () => {
   try {
@@ -69,6 +60,7 @@ const clickChatButton = () => {
         used={prdct.used}
         brand_id={prdct.brand_id}
         category_id={prdct.category_id}
+        img_urls ={prdct.img_urls}
         />
       ))}
           <div className="frame-wrapper">
